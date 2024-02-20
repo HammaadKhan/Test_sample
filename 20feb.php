75f38e8b6990c65c7ff29108f9d9199db468aed3
HammaadKhan <nogain447khan@gmail.com>
2024-02-20 19:49:23 +0500

    20feb

---
+++ 20feb.php
@@ -0,0 +1,241 @@
+<?php
+$PageTitle = "Partners";
+include_once("./layout/header.php");
+include($_SERVER['DOCUMENT_ROOT']. "/include/notified.php");
+
+?>
+
+<main class="pt-[5.5rem] lg:pt-24">
+    <!-- Benefits -->
+    <section class="relative pt-32 pb-24">
+        <picture class="pointer-events-none absolute inset-0 -z-10 dark:hidden">
+            <img src="./assets/front/img/gradient.jpg" alt="gradient" class="h-full w-full" />
+        </picture>
+        <picture class="pointer-events-none absolute inset-0 -z-10 hidden dark:block">
+            <img src="./assets/front/img/gradient_dark.jpg" alt="gradient dark" class="h-full w-full" />
+        </picture>
+        <div class="container">
+            <div class="mx-auto max-w-xl text-center">
+                <h1 class="mb-6 text-center font-display text-4xl font-medium text-jacarta-700 dark:text-white">
+                    Xhibiter Affiliate Partnership
+                </h1>
+                <p class="mb-16 text-lg leading-normal dark:text-jacarta-200">
+                    Interested in making money with Xhibiter? Earn A 20% Commission For Every Sale You Refer By
+                    Recommending
+                    The Most Powerful NFT marketplace.
+                </p>
+            </div>
+
+            <div class="grid gap-7 md:grid-cols-3">
+                <div
+                    class="rounded-2.5xl bg-white p-[3.75rem] text-center transition-shadow hover:shadow-xl dark:bg-jacarta-800">
+                    <div class="mb-6 inline-flex rounded-full bg-[#CDBCFF] p-2.5">
+                        <div
+                            class="inline-flex h-[4.25rem] w-[4.25rem] items-center justify-center rounded-full bg-accent">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                                class="h-[2.25rem] w-[2.25rem] fill-white">
+                                <path fill="none" d="M0 0h24v24H0z" />
+                                <path
+                                    d="M20 3l2 4v13a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.004L4 3h16zm0 6H4v10h16V9zm-8 1l4 4h-3v4h-2v-4H8l4-4zm6.764-5H5.236l-.999 2h15.527l-1-2z" />
+                            </svg>
+                        </div>
+                    </div>
+                    <h3 class="mx-auto mb-4 max-w-[9.625rem] font-display text-lg text-jacarta-700 dark:text-white">
+                        305,000+ NFTs created
+                    </h3>
+                </div>
+
+                <div
+                    class="rounded-2.5xl bg-white p-[3.75rem] text-center transition-shadow hover:shadow-xl dark:bg-jacarta-800">
+                    <div class="mb-6 inline-flex rounded-full bg-[#CDBCFF] p-2.5">
+                        <div
+                            class="inline-flex h-[4.25rem] w-[4.25rem] items-center justify-center rounded-full bg-accent">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                                class="h-[2.25rem] w-[2.25rem] fill-white">
+                                <path fill="none" d="M0 0h24v24H0z" />
+                                <path
+                                    d="M12 17l-5.878 3.59 1.598-6.7-5.23-4.48 6.865-.55L12 2.5l2.645 6.36 6.866.55-5.231 4.48 1.598 6.7L12 17zm0-2.344l2.817 1.72-.766-3.21 2.507-2.147-3.29-.264L12 7.708l-1.268 3.047-3.29.264 2.507 2.147-.766 3.21L12 14.657z" />
+                            </svg>
+                        </div>
+                    </div>
+                    <h3 class="mx-auto mb-4 max-w-[9.625rem] font-display text-lg text-jacarta-700 dark:text-white">
+                        Rated the best NFT Marketplace
+                    </h3>
+                </div>
+
+                <div
+                    class="rounded-2.5xl bg-white p-[3.75rem] text-center transition-shadow hover:shadow-xl dark:bg-jacarta-800">
+                    <div class="mb-6 inline-flex rounded-full bg-[#CDBCFF] p-2.5">
+                        <div
+                            class="inline-flex h-[4.25rem] w-[4.25rem] items-center justify-center rounded-full bg-accent">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                                class="h-[2.25rem] w-[2.25rem] fill-white">
+                                <path fill="none" d="M0 0h24v24H0z" />
+                                <path
+                                    d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-5-7h2a3 3 0 0 0 6 0h2a5 5 0 0 1-10 0zm1-2a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm8 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                            </svg>
+                        </div>
+                    </div>
+                    <h3 class="mx-auto mb-4 max-w-[9.625rem] font-display text-lg text-jacarta-700 dark:text-white">
+                        1.6M+ Happy NFT Users
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end benefits -->
+
+    <!-- Process -->
+    <section class="relative py-24">
+        <div class="container">
+            <div class="mx-auto mb-20 max-w-xl text-center">
+                <h2 class="mb-6 text-center font-display text-3xl font-medium text-jacarta-700 dark:text-white">
+                    How Our Affiliate Program Works
+                </h2>
+                <p class="dark:text-jacarta-300">
+                    While we take pride in being the first and largest marketplace and in our robust feature set, we
+                    also help
+                    our partners succeed with the following...
+                </p>
+            </div>
+
+            <div class="grid gap-7 md:grid-cols-3">
+                <div
+                    class="mb-12 rounded-2.5xl border border-jacarta-100 bg-white p-8 pt-0 text-center transition-shadow hover:shadow-xl dark:border-jacarta-600 dark:bg-jacarta-700">
+                    <div
+                        class="mb-9 -mt-8 inline-flex h-[5.5rem] w-[5.5rem] items-center justify-center rounded-full border border-jacarta-100 bg-white dark:border-jacarta-600 dark:bg-jacarta-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                            class="h-12 w-12 fill-accent">
+                            <path fill="none" d="M0 0h24v24H0z" />
+                            <path
+                                d="M17 16h2V4H9v2h8v10zm0 2v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3zM7 16v2h2v1h2v-1h.5a2.5 2.5 0 1 0 0-5h-3a.5.5 0 1 1 0-1H13v-2h-2V9H9v1h-.5a2.5 2.5 0 1 0 0 5h3a.5.5 0 1 1 0 1H7z" />
+                        </svg>
+                    </div>
+
+                    <h3 class="mb-4 font-display text-lg text-jacarta-700 dark:text-white">Generate Revenue</h3>
+                    <p class="dark:text-jacarta-300">
+                        With millions in transactions since 2017, we'll help you generate revenue on our platform ⁠—
+                        from direct
+                        sales to secondary sales
+                    </p>
+                </div>
+                <div
+                    class="mb-12 rounded-2.5xl border border-jacarta-100 bg-white p-8 pt-0 text-center transition-shadow hover:shadow-xl dark:border-jacarta-600 dark:bg-jacarta-700">
+                    <div
+                        class="mb-9 -mt-8 inline-flex h-[5.5rem] w-[5.5rem] items-center justify-center rounded-full border border-jacarta-100 bg-white dark:border-jacarta-600 dark:bg-jacarta-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                            class="h-12 w-12 fill-accent">
+                            <path fill="none" d="M0 0h24v24H0z" />
+                            <path
+                                d="M12 11a5 5 0 0 1 5 5v6H7v-6a5 5 0 0 1 5-5zm-6.712 3.006a6.983 6.983 0 0 0-.28 1.65L5 16v6H2v-4.5a3.5 3.5 0 0 1 3.119-3.48l.17-.014zm13.424 0A3.501 3.501 0 0 1 22 17.5V22h-3v-6c0-.693-.1-1.362-.288-1.994zM5.5 8a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5zm13 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5zM12 2a4 4 0 1 1 0 8 4 4 0 0 1 0-8z" />
+                        </svg>
+                    </div>
+
+                    <h3 class="mb-4 font-display text-lg text-jacarta-700 dark:text-white">Reach Millions</h3>
+                    <p class="dark:text-jacarta-300">
+                        With over thousands of users and followers, we can help you reach a large audience of collectors
+                    </p>
+                </div>
+                <div
+                    class="mb-12 rounded-2.5xl border border-jacarta-100 bg-white p-8 pt-0 text-center transition-shadow hover:shadow-xl dark:border-jacarta-600 dark:bg-jacarta-700">
+                    <div
+                        class="mb-9 -mt-8 inline-flex h-[5.5rem] w-[5.5rem] items-center justify-center rounded-full border border-jacarta-100 bg-white dark:border-jacarta-600 dark:bg-jacarta-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                            class="h-12 w-12 fill-accent">
+                            <path fill="none" d="M0 0h24v24H0z" />
+                            <path
+                                d="M22 6h-7a6 6 0 1 0 0 12h7v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2zm-7 2h8v8h-8a4 4 0 1 1 0-8zm0 3v2h3v-2h-3z" />
+                        </svg>
+                    </div>
+
+                    <h3 class="mb-4 font-display text-lg text-jacarta-700 dark:text-white">Spend Less</h3>
+                    <p class="dark:text-jacarta-300">
+                        Compared with other NFT marketplaces, we have lowest fees in the space allowing you to spend
+                        less with
+                        your created collection
+                    </p>
+                </div>
+                <div
+                    class="mb-12 rounded-2.5xl border border-jacarta-100 bg-white p-8 pt-0 text-center transition-shadow hover:shadow-xl dark:border-jacarta-600 dark:bg-jacarta-700">
+                    <div
+                        class="mb-9 -mt-8 inline-flex h-[5.5rem] w-[5.5rem] items-center justify-center rounded-full border border-jacarta-100 bg-white dark:border-jacarta-600 dark:bg-jacarta-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                            class="h-12 w-12 fill-accent">
+                            <path fill="none" d="M0 0h24v24H0z" />
+                            <path
+                                d="M22 20v2H2v-2h1v-6.758A4.496 4.496 0 0 1 1 9.5c0-.827.224-1.624.633-2.303L4.345 2.5a1 1 0 0 1 .866-.5H18.79a1 1 0 0 1 .866.5l2.702 4.682A4.496 4.496 0 0 1 21 13.242V20h1zM5.789 4L3.356 8.213a2.5 2.5 0 0 0 4.466 2.216c.335-.837 1.52-.837 1.856 0a2.5 2.5 0 0 0 4.644 0c.335-.837 1.52-.837 1.856 0a2.5 2.5 0 1 0 4.457-2.232L18.21 4H5.79z" />
+                        </svg>
+                    </div>
+
+                    <h3 class="mb-4 font-display text-lg text-jacarta-700 dark:text-white">Gas-free Marketplace</h3>
+                    <p class="dark:text-jacarta-300">
+                        With our multiple blockchains support, create, buy, and sell NFTs without paying any gas fee
+                    </p>
+                </div>
+                <div
+                    class="mb-12 rounded-2.5xl border border-jacarta-100 bg-white p-8 pt-0 text-center transition-shadow hover:shadow-xl dark:border-jacarta-600 dark:bg-jacarta-700">
+                    <div
+                        class="mb-9 -mt-8 inline-flex h-[5.5rem] w-[5.5rem] items-center justify-center rounded-full border border-jacarta-100 bg-white dark:border-jacarta-600 dark:bg-jacarta-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                            class="h-12 w-12 fill-accent">
+                            <path fill="none" d="M0 0h24v24H0z" />
+                            <path d="M13 9h8L11 24v-9H4l9-15v9zm-2 2V7.22L7.532 13H13v4.394L17.263 11H11z" />
+                        </svg>
+                    </div>
+
+                    <h3 class="mb-4 font-display text-lg text-jacarta-700 dark:text-white">Advertise Xhibiter</h3>
+                    <p class="dark:text-jacarta-300">
+                        With the most powerful way for users to buy and sell NFTS, we offer the most advanced features
+                    </p>
+                </div>
+                <div
+                    class="mb-12 rounded-2.5xl border border-jacarta-100 bg-white p-8 pt-0 text-center transition-shadow hover:shadow-xl dark:border-jacarta-600 dark:bg-jacarta-700">
+                    <div
+                        class="mb-9 -mt-8 inline-flex h-[5.5rem] w-[5.5rem] items-center justify-center rounded-full border border-jacarta-100 bg-white dark:border-jacarta-600 dark:bg-jacarta-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                            class="h-12 w-12 fill-accent">
+                            <path fill="none" d="M0 0h24v24H0z" />
+                            <path
+                                d="M9.33 11.5h2.17A4.5 4.5 0 0 1 16 16H8.999L9 17h8v-1a5.578 5.578 0 0 0-.886-3H19a5 5 0 0 1 4.516 2.851C21.151 18.972 17.322 21 13 21c-2.761 0-5.1-.59-7-1.625L6 10.071A6.967 6.967 0 0 1 9.33 11.5zM5 19a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-9a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v9zM18 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm-7-3a3 3 0 1 1 0 6 3 3 0 0 1 0-6z" />
+                        </svg>
+                    </div>
+
+                    <h3 class="mb-4 font-display text-lg text-jacarta-700 dark:text-white">Earn 20% Commission Per Sale!
+                    </h3>
+                    <p class="dark:text-jacarta-300">
+                        With over thousands of collections, we are proud to host the widest range of categories, ranging
+                        from
+                        digital to physical NFTs
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end process -->
+
+    
+
+    <!-- CTA -->
+    <section class="bg-accent py-28">
+        <div class="container">
+            <div class="mx-auto max-w-2xl text-center">
+                <h2 class="mx-auto mb-8 max-w-sm font-display text-3xl text-white">
+                    Interested In Becoming An Affiliate Partner?
+                </h2>
+                <p class="mb-10 text-lg leading-normal text-white">
+                    Join our mailing list to stay in the loop with our newest feature releases, NFT drops, and tips and
+                    tricks
+                    for navigating Xhibiter
+                </p>
+                <a href="login"
+                    class="inline-block rounded-full bg-white py-3 px-8 text-center font-semibold text-accent transition-all hover:bg-accent-dark hover:text-white">Sign
+                    Up Now</a>
+            </div>
+        </div>
+    </section>
+    <!-- end cta -->
+
+    <?php
+  include_once("./layout/footer.php");
+
+  ?>
